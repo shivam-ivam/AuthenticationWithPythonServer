ab43fb44464dc1937605e4b102a22bac154a31fe
Shivam-ivam1 <sdjfjs>
2022-11-17 12:59:43 +0530

    Level-1 authentication

---
+++ main.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////study/python_bootcamp_2/Day68_authentication_login_page/users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+## CREATE TABLE IN DB
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+#Line below only required once, when creating DB.
+
+# db.create_all()
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        with app.app_context():
+            new_user = User(
+                email=request.form["email"],
+                password=request.form["password"],
+                name=request.form["name"]
+            )
+            db.session.add(new_user)
+            db.session.commit()
+        return render_template("secrets.html", name=request.form["name"])
+    return render_template("register.html")
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        data = User.query.filter_by(email=request.form["email"]).first()
+        if data is not None:
+            if request.form["password"] == data.password:
+                return render_template("secrets.html", name=data.name)
+            else:
+                return "<h1>Authentication failed!</h1>" \
+                       "<p>Password is wrong</p>"
+        else:
+            return redirect("/register")
+    return render_template("login.html")
+
+
+@app.route('/secrets')
+def secrets():
+    return render_template("secrets.html")
+
+
+@app.route('/logout')
+def logout():
+    pass
+
+
+@app.route('/download')
+def download():
+    return send_from_directory('static', filename="files/cheat_sheet.pdf")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
